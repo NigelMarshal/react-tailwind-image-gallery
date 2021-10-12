0b59c79b4cbb2d911771e0c17b5f22cf2a9edc45
Nigel Marshal <nigel566@hotmail.com>
2021-10-12 09:24:14 +0400

    feat(search): Implemented search functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import ImageCard from "./components/ImageCard";
+import ImageSearch from "./components/ImageSearch";
 
 function App() {
   const [images, setImages] = useState([]);
@@ -16,10 +17,15 @@ function App() {
         setIsLoading(false);
       })
       .catch((err) => console.log(err));
-  }, []);
+  }, [term]);
 
   return (
     <div className="container mx-auto">
+      <ImageSearch searchText={(text) => setTerm(text)} />
+      {!isLoading && images.length === 0 && <h1 className="text-5xl text-center mx-auto mt-32">
+        No images found! :(
+      </h1>}
+
       {isLoading ? <h1 className="text-6xl text-center mx-auto mt-32">
         Loading...
       </h1> : <div className="grid grid-cols-3 gap-4">
